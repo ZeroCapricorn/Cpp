2af85092c3983a365801555868db59067587f0d4
ZeroCapricorn <165401370+ZeroCapricorn@users.noreply.github.com>
2024-08-22 17:07:03 +0800

    Add files via upload

---
+++ a_helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    system("chcp 65001");
+    cout << "hello world" << endl;
+    cout << "哈哈" << endl;
+    system("pause");
+    return 0;
+}
